f217cad8b0142f50c79f42bccd58386505e6c30b
Martin Balak <martin.balak@applifting.cz>
2014-10-18 11:10:10 +0200

    imagebutton

--- src/Xamarin.Forms.Labs/Xamarin.Forms.Labs.Droid/Controls/ImageButton/ImageButtonRenderer.cs
+++ src/Xamarin.Forms.Labs/Xamarin.Forms.Labs.Droid/Controls/ImageButton/ImageButtonRenderer.cs
@@ -152,10 +152,21 @@ namespace Xamarin.Forms.Labs.Droid.Controls.ImageButton
         /// <returns>A scaled <see cref="Drawable"/>.</returns>
         private Drawable GetScaleDrawable(Drawable drawable, int width, int height)
         {
-            var returnValue = new ScaleDrawable(drawable, 0, width, height).Drawable;
+            //var returnValue = new ScaleDrawable(drawable, 0, width, height).Drawable;
+			var returnValue = new ScaleDrawable(drawable, 0, RequestToPixels(width), RequestToPixels(height)).Drawable;
             returnValue.SetBounds(0, 0, width, height);
             return returnValue;
         }
+
+		 /// <summary>
+		/// Returns a drawable dimension modified according to the current display DPI.
+		 /// </summary>
+		 /// <param name="sizeRequest">The requested size in relative units.</param>
+		 /// <returns>Size in pixels.</returns>
+		 public int RequestToPixels(int sizeRequest)
+		 {
+			 return (int) (sizeRequest * Resources.DisplayMetrics.Density);
+			 }
 
 
 		//Hot fix for the layout positioning issue on Android as described in http://forums.xamarin.com/discussion/20608/fix-for-button-layout-bug-on-android
